44b60a2201f0f4fb61f8f498afea6cce81636dd9
ahenault <ahenault@student.42.fr>
2024-11-05 18:27:22 +0100

    changed flags for echo

--- src/builtins/echo.c
+++ src/builtins/echo.c
@@ -6,7 +6,7 @@
 /*   By: ahenault <ahenault@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/23 18:19:17 by ahenault          #+#    #+#             */
-/*   Updated: 2024/10/15 21:47:56 by ahenault         ###   ########.fr       */
+/*   Updated: 2024/11/05 18:26:40 by ahenault         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,12 +15,19 @@
 int	ft_echo(t_command *command)
 {
 	int	i;
+	int	j;
 	int	new_line;
 
 	i = 1;
 	new_line = 0;
-	if (command->args[1] && ft_strncmp(command->args[1], "-n", 3) == 0)
+	while (command->args[i] && command->args[i][0] == '-'
+		&& command->args[i][1] == 'n')
 	{
+		j = 1;
+		while (command->args[i][j] == 'n')
+			j++;
+		if (command->args[i][j] != '\0')
+			break ;
 		i++;
 		new_line = 1;
 	}
