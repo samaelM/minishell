82ef42fdc594c1bcb4a19a535e84a30271ad34fe
ahenault <ahenault@student.42.fr>
2024-11-11 19:38:55 +0100

    changed command struct with tmp

--- src/exec/exec_pipe_cmds.c
+++ src/exec/exec_pipe_cmds.c
@@ -6,7 +6,7 @@
 /*   By: ahenault <ahenault@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/06 18:52:13 by ahenault          #+#    #+#             */
-/*   Updated: 2024/11/11 19:23:10 by ahenault         ###   ########.fr       */
+/*   Updated: 2024/11/11 19:29:57 by ahenault         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,45 +14,45 @@
 
 void	dup_infile(t_global *g, int i)
 {
-	if (g->command->infile != -1)
-		dup2(g->command->infile, 0);
+	if (g->tmp->infile != -1)
+		dup2(g->tmp->infile, 0);
 	else if (i != 0)
 	{
-		dup2(g->command->prev_fd, 0);
-		close(g->command->prev_fd);
+		dup2(g->tmp->prev_fd, 0);
+		close(g->tmp->prev_fd);
 	}
 }
 
 void	dup_outfile(t_global *g)
 {
-	if (g->command->outfile != -1)
-		dup2(g->command->outfile, 1);
-	else if (g->command->next)
+	if (g->tmp->outfile != -1)
+		dup2(g->tmp->outfile, 1);
+	else if (g->tmp->next)
 	{
-		dup2(g->command->pipe[1], 1);
-		close(g->command->pipe[1]);
+		dup2(g->tmp->pipe[1], 1);
+		close(g->tmp->pipe[1]);
 	}
 	else
-		close(g->command->pipe[1]);
+		close(g->tmp->pipe[1]);
 }
 
 int	exec_which_cmd_pipe(t_global *g)
 {
 	if (check_is_cmd_is_ok(g) == 0)
 	{
-		if (ft_strcmp(g->command->args[0], "exit") == 0)
+		if (ft_strcmp(g->tmp->args[0], "exit") == 0)
 			g->exit_value = ft_exit(g);
-		else if (ft_strcmp(g->command->args[0], "pwd") == 0)
+		else if (ft_strcmp(g->tmp->args[0], "pwd") == 0)
 			g->exit_value = ft_pwd();
-		else if (ft_strcmp(g->command->args[0], "cd") == 0)
+		else if (ft_strcmp(g->tmp->args[0], "cd") == 0)
 			g->exit_value = ft_cd(g);
-		else if (ft_strcmp(g->command->args[0], "echo") == 0)
-			g->exit_value = ft_echo(g->command);
-		else if (ft_strcmp(g->command->args[0], "env") == 0)
+		else if (ft_strcmp(g->tmp->args[0], "echo") == 0)
+			g->exit_value = ft_echo(g->tmp);
+		else if (ft_strcmp(g->tmp->args[0], "env") == 0)
 			g->exit_value = ft_env(g);
-		else if (ft_strcmp(g->command->args[0], "export") == 0)
+		else if (ft_strcmp(g->tmp->args[0], "export") == 0)
 			g->exit_value = ft_export(g);
-		else if (ft_strcmp(g->command->args[0], "unset") == 0)
+		else if (ft_strcmp(g->tmp->args[0], "unset") == 0)
 			g->exit_value = ft_unset(g);
 		else
 			execve_cmd(g);
@@ -65,14 +65,14 @@ int	pipe_and_fork(t_global *g, int i)
 {
 	pid_t	pid;
 
-	if (pipe(g->command->pipe) == -1)
+	if (pipe(g->tmp->pipe) == -1)
 		return (1);
 	pid = fork();
 	if (pid == -1)
 		ft_perrorf("erreur fork");
 	else if (pid == 0) // fils
 	{
-		close(g->command->pipe[0]);
+		close(g->tmp->pipe[0]);
 		dup_infile(g, i);
 		dup_outfile(g);
 		exec_which_cmd_pipe(g);
@@ -86,24 +86,23 @@ void	exec_pipe_cmds(t_global *g)
 
 	i = 0;
 	g->tmp = g->command;
-	while (g->command)
+	while (g->tmp)
 	{
 		pipe_and_fork(g, i);
 		i++;
-		close(g->command->pipe[1]);
-		if (g->command->next)
+		close(g->tmp->pipe[1]);
+		if (g->tmp->next)
 		{
-			g->command->next->prev_fd = g->command->pipe[0];
-			if (g->command->prev_fd != -1)
-				close(g->command->prev_fd);
-			g->command = g->command->next;
+			g->tmp->next->prev_fd = g->tmp->pipe[0];
+			if (g->tmp->prev_fd != -1)
+				close(g->tmp->prev_fd);
+			g->tmp = g->tmp->next;
 		}
 		else
 			break ;
 	}
-	close(g->command->pipe[0]);
-	if (g->command->prev_fd != -1)
-		close(g->command->prev_fd);
-	g->command = g->tmp;
+	close(g->tmp->pipe[0]);
+	if (g->tmp->prev_fd != -1)
+		close(g->tmp->prev_fd);
 	ft_waitall(g);
 }
