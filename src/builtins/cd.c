145ea70637db6302ba32988e3208a80fe7e10ad9
ahenault <ahenault@student.42.fr>
2024-11-15 13:59:05 +0100

    changed cd

--- src/builtins/cd.c
+++ src/builtins/cd.c
@@ -6,7 +6,7 @@
 /*   By: ahenault <ahenault@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/21 14:30:33 by maemaldo          #+#    #+#             */
-/*   Updated: 2024/11/14 21:12:38 by ahenault         ###   ########.fr       */
+/*   Updated: 2024/11/15 13:55:19 by ahenault         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,12 +19,12 @@ char	*ft_getcwd(void)
 	cwd = malloc(PATH_MAX);
 	if (!cwd)
 	{
-		ft_perrorf("error with malloc\n");
+		ft_perrorf("change pwd variable: error with malloc\n");
 		return (NULL);
 	}
 	if (getcwd(cwd, PATH_MAX) == NULL)
 	{
-		ft_perrorf("error with getcwd\n");
+		ft_perrorf("change pwd variable: error with getcwd\n");
 		free(cwd);
 		return (NULL);
 	}
@@ -64,6 +64,8 @@ int	change_pwd_vars(t_global *g, char *cwd)
 	char	*string;
 	int		is_line;
 
+	if (!cwd)
+		return (1);
 	string = malloc(sizeof(char) * (8 + ft_strlen(cwd) + 1));
 	if (!string)
 	{
@@ -90,13 +92,15 @@ int	cd_options(t_global *g, char *var, char *cwd)
 	path = ft_getenv(g->env, var);
 	if (!path)
 	{
-		free(cwd);
+		if (cwd)
+			free(cwd);
 		ft_perrorf("cd: %s not set\n", var);
 		return (1);
 	}
 	if (chdir(path))
 	{
-		free(cwd);
+		if (cwd)
+			free(cwd);
 		ft_perrorf("cd: %s: %s\n", path, strerror(errno));
 		return (1);
 	}
@@ -109,22 +113,20 @@ int	ft_cd(t_global *g)
 {
 	char	*cwd;
 
-	cwd = ft_getcwd();
-	if (!cwd)
-		return (1);
 	if (g->tmp->args[1] && g->tmp->args[2])
 	{
-		free(cwd);
 		ft_perrorf("cd: too many arguments\n");
 		return (1);
 	}
+	cwd = ft_getcwd();
 	if (g->tmp->args[1] == NULL)
 		return (cd_options(g, "HOME", cwd));
 	if (ft_strcmp(g->tmp->args[1], "-") == 0)
 		return (cd_options(g, "OLDPWD", cwd));
 	if (g->tmp->args[1] && chdir(g->tmp->args[1]))
 	{
-		free(cwd);
+		if (cwd)
+			free(cwd);
 		ft_perrorf("cd: %s: %s\n", g->tmp->args[1], strerror(errno));
 		return (1);
 	}
